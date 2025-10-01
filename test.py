c02d562c2fc28ee23a0350ae806b36ace7c61712
Amalie Holten <amalie.holten.nielsen@outlook.dk>
2025-10-01 16:42:35 +0200

    New ideas for tasks

--- test.py
+++ test.py
@@ -1,4 +1,70 @@
+# til print opgaverne skal vi vise at de kan plusse, minus, gange, dividere og potens
+print(4 + 6)
+print(15 - 7)
+print(3 * 4)
+print(20 / 5)
+print(5**2)
+print(7 * 4 + 6) # matematik virker som normalt :P
+
+# man kan lave variabler
+alder = 12 # tal
+navn = "Magnus" # tekst
+sej = True # sandt/falsk
+
+# man kan navngive variabler næsten alt.
+guhijok = "jkhugyuh"
+fghj = 45678
+hjkh = False
+
+# det må ikke starte med et tal eller indeholde mellemrum
+#6jejj = 567
+#hj khj = "ko"
+
+# her viser vi at en variabel er den indhold
+tal = 5
+print(tal, "Her står 5")
+print(5, "her står også 5")
+
+# man kan printe variabler på flere måder
+alder = 12
+navn = "Magnus"
+sej = True
+arbejde = "Avis bud"
+klasse_trin = 6
+
+print(navn)
+print(alder)
+print(sej)
+
+# man kan formatere en string med variabler
+print(navn, "er", alder, "år gammel og han går i", klasse_trin, "klasse. Når han ikke er i skole arbejder han som", arbejde)
+
+# Man kan regne indholdet af en variabel
+tal1 = 17
+tal2 = 5 + 2
+
+# man kan regne med variabler
+tal3 = tal1 + tal2
+
+# man kan pl
+tal4 = tal1 + 5
+
+# rækkefølge
+rf = "kian er røøøv"
 
 
-for i in range(0, 11, 2):
-    print(i)
+# det samme som ovenover
+tal += 1
+tal -= 1
+tal *= 2
+tal /= 2
+tal **= 2
+
+# man kan gøre en variabel højere
+tal = tal + 5
+#man kan gøre en variabel lavere
+tal = tal - 5
+
+crazy = "nisse"
+crazy += "dils"
+print(crazy)
